27d310e6a9cc6536e143e83fa4931d2afada1cea
Alexander Pisarenko <yakimbee@gmail.com>
2020-10-21 16:42:09 +0300

    update extraction

--- fastfm2/als/__init__.py
+++ fastfm2/als/__init__.py
@@ -1,12 +1,10 @@
 try:
-    from . import regression
     from .regression import FMRegression
 except ImportError:
     print("NO REGRESSION MODULE EXISTS!")
     pass
 
 try:
-    from . import classification
     from .classification import FMClassification
 except ImportError:
     print("NO CLASSIFICATION MODULE EXISTS!")
