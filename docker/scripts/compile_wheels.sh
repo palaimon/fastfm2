7c5b4a5ee2436d2940b2f4adc25b824e91881c9d
Alexander Pisarenko <yakimbee@gmail.com>
2021-01-11 01:59:01 +0200

    extract++

--- docker/scripts/compile_wheels.sh
+++ docker/scripts/compile_wheels.sh
@@ -2,14 +2,14 @@
 
 for PYBIN in /opt/python/*/bin;
 	do
-      "${PYBIN}/pip" wheel --no-deps -w wheelhouse/ src/
-      "${PYBIN}/pip" install --no-index --no-cache-dir --find-links=./wheelhouse fastfm2
+      "${PYBIN}/pip" wheel -w wheelhouse/ src/
+      "${PYBIN}/pip" install --no-index --no-cache-dir --find-links=/wheelhouse fastfm
       "${PYBIN}/python" -m pytest src/fastfm2
 done
 
-for whl in wheelhouse/*.whl
+for whl in wheelhouse/fastfm*.whl
 	do  auditwheel show "$whl"
 	    auditwheel repair "$whl"
-	    rm "$whl"
 done
 
+rclone copy wheelhouse/fasfm*.whl data:palaimon-releases/fastfm/wheelhouse
